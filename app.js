6d0f517efc35e01f6f66c0d9809fc1383344a997
KMicka <kleidimicka13@gmail.com>
2026-07-24 14:31:33 +0200

    Update fuel prices 2026-07-24 12:31

--- app.js
+++ app.js
@@ -379,7 +379,7 @@ const defaultHomeFuelPriceBoard = {
     en: "Tirana"
   },
   exchangeRate: 92,
-  updatedAt: "2026-07-24T12:31:27+00:00",
+  updatedAt: "2026-07-24T12:31:31+00:00",
   history: [
     {
       date: "2026-06-01",
@@ -425,7 +425,7 @@ const defaultHomeFuelPriceBoard = {
         ev: 38
       }
     },
-                            {
+                                {
       date: "2026-07-24",
       prices: {
         "100": 206,
