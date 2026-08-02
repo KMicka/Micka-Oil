cf9fd2770f56ba8f356989ed07f003035293cbf0
KMicka <kleidimicka13@gmail.com>
2026-08-02 10:08:36 +0200

    Update fuel prices 2026-08-02 08:08

--- app.js
+++ app.js
@@ -379,7 +379,7 @@ const defaultHomeFuelPriceBoard = {
     en: "Tirana"
   },
   exchangeRate: 92,
-  updatedAt: "2026-08-01T13:40:47+00:00",
+  updatedAt: "2026-08-02T08:08:33+00:00",
   history: [
     {
       date: "2026-06-01",
@@ -446,6 +446,17 @@ const defaultHomeFuelPriceBoard = {
         lpg: 69,
         ev: 38
       }
+    },
+    {
+      date: "2026-08-02",
+      prices: {
+        "100": 214,
+        "95": 199,
+        diesel: 211,
+        "diesel-shell": 224,
+        lpg: 69,
+        ev: 38
+      }
     }
   ],
   products: [
@@ -461,7 +472,7 @@ const defaultHomeFuelPriceBoard = {
         sq: "Performancë premium dhe fuqi.",
         en: "Premium performance and power."
       },
-      price: 211
+      price: 214
     },
     {
       id: "95",
